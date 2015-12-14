5cd78c24740b89c8dbcfe728c4c4bad71693e779
Brandon Olin <brandon@devblackops.io>
2015-12-13 21:01:30 -0800

    Remove old code

--- POSHOrigin.psm1
+++ POSHOrigin.psm1
@@ -11,14 +11,10 @@ $moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
 
 Export-ModuleMember -Function *POSH*
 New-Alias -Name gpoc -Value Get-POSHOriginConfig
-#New-Alias -Name gpocnew -Value Get-POSHOriginConfigNEW
 New-Alias -Name gpd -Value Get-POSHDefault
 New-Alias -Name ipo -Value Invoke-POSHOrigin
-New-Alias -Name iponew -Value Invoke-POSHOriginNEW
 New-Alias -Name resource -Value New-POSHOriginResource
 Export-ModuleMember -Alias gpoc
-#Export-ModuleMember -Alias gpocnew
 Export-ModuleMember -Alias gpd
 Export-ModuleMember -Alias ipo
-Export-ModuleMember -Alias iponew
 Export-ModuleMember -Alias resource
