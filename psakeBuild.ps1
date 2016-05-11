533d3f49ab0ce345ac4851d3258618eb7e30346d
Brandon Olin <brandon@devblackops.io>
2016-05-10 23:21:37 -0700

    Add build / deploy scripts

---
+++ psakeBuild.ps1
@@ -0,0 +1,26 @@
+properties {
+    $sut = '.\POSHOrigin'
+    $tests = '.\Tests'
+}
+
+task default -depends Analyze, Test
+
+task Analyze {
+    $saResults = Invoke-ScriptAnalyzer -Path $sut -Severity Error -Recurse -Verbose:$false
+    if ($saResults) {
+        $saResults | Format-Table  
+        Write-Error -Message 'One or more Script Analyzer errors/warnings where found. Build cannot continue!'        
+    }
+}
+
+task Test {
+    $testResults = Invoke-Pester -Path $tests -PassThru
+    if ($testResults.FailedCount -gt 0) {
+        $testResults | Format-List
+        Write-Error -Message 'One or more Pester tests failed. Build cannot continue!'
+    }
+}
+
+task Deploy -depends Analyze, Test {
+    Invoke-PSDeploy -Path '.\psgallery.psdeploy.ps1' -Force -Verbose:$VerbosePreference
+}
