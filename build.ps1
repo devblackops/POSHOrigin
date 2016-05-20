4385bbdc1e215e1baa71ac5436b19ed9f80deb79
brandon@devblackops.io <brandon@devblackops.io>
2016-05-20 15:50:52 -0700

    Add PSScriptAnalyzer to build dependencies

--- build.ps1
+++ build.ps1
@@ -6,5 +6,6 @@ param(
 if (!(Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Scope CurrentUser }
 if (!(Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -Scope CurrentUser }
 if (!(Get-Module -Name PSDeploy -ListAvailable)) { Install-Module -Name PSDeploy -Scope CurrentUser }
+if (!(Get-Module -Name PSScriptAnalyzer -ListAvailable)) { Install-Module -Name PSDeploy -Scope CurrentUser }
 
 Invoke-psake -buildFile "$PSScriptRoot\psakeBuild.ps1" -taskList $Task -nologo -Verbose:$VerbosePreference
