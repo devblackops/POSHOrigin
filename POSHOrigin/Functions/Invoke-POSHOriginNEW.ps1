b5002db8d3d0df227adae8226b30083ea6c9340a
Brandon Olin <brandon@devblackops.io>
2016-05-22 14:01:55 -0700

    Remove -NoTranslate from comment based help

--- POSHOrigin/Functions/Invoke-POSHOriginNEW.ps1
+++ POSHOrigin/Functions/Invoke-POSHOriginNEW.ps1
@@ -11,8 +11,6 @@ function Invoke-POSHOriginNEW {
             ** THIS IS AN EXPERIMENTAL CMDLET AND MAY BE SIGNIFICANTLY MODIFIED IN FUTURE VERSIONS **
         .PARAMETER InputObject
             One or more custom objects containing the required options for the DSC resource to be provisioned.
-        .PARAMETER NoTranslate
-            Do not call the Invoke.ps1 script from the DSC resource module. Instead pass the object directly to Invoke-DscResource without translation.
         .PARAMETER WhatIf
             Only execute the TEST functionality of DSC.
             
